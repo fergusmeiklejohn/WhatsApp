7e8aca4ede9e7d228e9bb25a224aa044602f15f7
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2022-10-27 13:00:06 +0800

    building

--- App.tsx
+++ App.tsx
@@ -4,6 +4,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Navigator from "./app/navigation/navigator";
 
+import { Amplify } from "aws-amplify";
+import awsconfig from "./app/aws-exports";
+
+Amplify.configure({ awsconfig, Analytics: { disabled: true } });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -12,12 +25,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingVertical: 50,
-    justifyContent: "center",
-  },
-});
